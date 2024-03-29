ea24073aee0394d55cc9ea2723a84850111f9a62
Semicolon <163368381+semicolon-10@users.noreply.github.com>
2024-03-29 15:54:28 +0800

    add publisher consumer

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,36 @@
+use amiquip::{Connection, ConsumerMessage, ConsumerOptions, Exchange, Publish, QueueDeclareOptions};
 
+fn main() -> amiquip::Result<()> {
+    // Setup Rabbit-MQ
+    let mut connection =
+        Connection::insecure_open("amqp://guest:guest@localhost:5672")?;
+
+    let channel = connection.open_channel(None)?;
+
+    let exchange = Exchange::direct(&channel);
+    let queue =
+        channel.queue_declare("hello", QueueDeclareOptions::default())?;
+
+    exchange.publish(Publish::new(b"hello world","hello"))?;
+
+    let consumer = queue.consume(ConsumerOptions::default())?;
+
+    println!("Press ctrl-c to exit");
+
+    for (i, message)
+    in consumer.receiver().iter().enumerate() {
+        match message {
+            ConsumerMessage::Delivery(delivery) => {
+                let body = String::from_utf8_lossy(&delivery.body);
+                println!("Received message number {} with body {}",i, body);
+                consumer.ack(delivery)?;
+            }
+            other => {
+                println!("Consumer ended {:?}", other);
+                break;
+            }
+        }
+    }
+
+    connection.close()
+}
